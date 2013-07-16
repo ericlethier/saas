6c9066bf71db6f35b0cacf8b422cf2c9dbec046f
e.lethier@gmail.com <el186003@WCZEL186003-7CR.TD.TERADATA.COM>
2013-07-16 17:08:30 +0400

    Final version

--- hw1/hw1/part3.rb
+++ hw1/hw1/part3.rb
@@ -1,3 +1,34 @@
-def combine_anagrams(words)
- # YOUR CODE HERE
+def sort_word(word)
+	return word.downcase.split('').sort.join
 end
+
+def combine_anagrams(words)
+
+	if words.length == 0
+		return Array.new
+	end
+
+	output = Array.new {Array.new} 
+	
+	words.each do|word|
+		sorted_word = sort_word(word)
+
+		word_exist = false
+		output.each do|row|
+			if (sort_word(row[0]) == sorted_word)
+				row.push(word)
+				word_exist = true
+				break
+			end
+		end
+		
+		if word_exist == false
+			output[output.length] = Array[word]
+		end
+		
+	end
+	return output
+end
+
+
+
