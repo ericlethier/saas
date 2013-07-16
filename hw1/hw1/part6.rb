967d7c691e09bd5160e1c0b7d9effd08768c5311
e.lethier@gmail.com <el186003@WCZEL186003-7CR.TD.TERADATA.COM>
2013-07-16 18:38:02 +0400

    Final version

--- hw1/hw1/part6.rb
+++ hw1/hw1/part6.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'dollar' => 1, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,12 +8,35 @@ class Numeric
       super
     end
   end
+  
+  def in(currency)
+	  singular_currency = currency.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self / @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+  
 end
 
 class String
-  # YOUR CODE HERE
+  def palindrome?
+		if (self.downcase.gsub(/[^[:alnum:]]/, "") == self.downcase.reverse.gsub(/[^[:alnum:]]/, ""))
+			return true
+		else
+			return false
+		end
+	end
 end
 
 module Enumerable
-  # YOUR CODE HERE
+	def palindrome?
+		if (self.to_a == self.to_a.reverse)
+			return true
+		else
+			return false
+		end
+	end
 end
+
