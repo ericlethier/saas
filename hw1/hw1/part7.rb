3704460067f860100b27ec0104689f5083a555bd
e.lethier@gmail.com <el186003@WCZEL186003-7CR.TD.TERADATA.COM>
2013-07-17 11:24:23 +0400

    Final version

--- hw1/hw1/part7.rb
+++ hw1/hw1/part7.rb
@@ -1,4 +1,26 @@
 class CartesianProduct
   include Enumerable
-  # YOUR CODE HERE
+  
+  attr_accessor :array1
+  attr_accessor :array2
+  
+  def initialize(array1, array2)
+	if array1.length == 0 || array2.length == 0
+		array1 = Array.new
+		array2 = Array.new
+	end
+	
+    @array1 = array1
+	@array2 = array2
+  end
+  
+  def each
+    @array1.each do |item1|
+		@array2.each do |item2|
+			yield [item1, item2]
+		end
+    end
+  end
+  
 end
+
